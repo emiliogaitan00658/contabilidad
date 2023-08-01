c29bc7876d9be29edacb710c90ae84a08af15831
emilio antonio gaitan fues <emiliogaitan00658@gmail.com>
2023-08-01 00:05:34 -0600

    servidor

--- detalles_credito.php
+++ detalles_credito.php
@@ -35,6 +35,30 @@ if ($_GET) {
 //    echo '<script> location.href="buscar_clientes.php" </script>';
 //}
 if ($_POST) {
+
+    $rr=datos_clientes::idcliente_credito($key,$mysqli);
+
+    if($rr!="false"){
+        echo '<script>
+ swal({
+   title: "Anuncio ?",
+   text: "Ya contiene un credito",
+   icon: "warning",
+   buttons: true,
+
+ })
+ .then((willDelete) => {
+   if (willDelete) {
+     location.href="factura_dia.php";
+   }else {
+     location.href="factura_dia.php";
+   }
+ });
+ </script>';
+    }
+
+
+
     $producto = strtoupper($_POST['textproducto']);
     $inicio = $_POST['textfechainicio'];
     $monto = $_POST['textpagar'];
